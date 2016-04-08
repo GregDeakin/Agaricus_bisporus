71d73c1e619530ac9269eb62ce63fb3024ca1bb6
Greg Deakin <GregDeakin@users.noreply.github.com>
2016-04-08 14:29:52 +0100

    Create submit_varcount.sh

---
+++ submit_varcount.sh
@@ -0,0 +1,18 @@
+
+REF=$1
+SAMPLE=$2
+
+bowtie2 -p 8 --no-unal -x $REF -U $SAMPLE -S $REF.sam
+samtools view -S -b $REF.sam >$REF.bam
+samtools sort $REF.bam $REF.sorted
+samtools mpileup -o $REF.pile.vcf -v -t DPR -u -f $REF.txt $REF.sorted.bam
+bcftools call -Ov -v -m $REF.pile.vcf > $REF.vars.vcf
+
+grep ^[^#] $REF.vars.vcf|awk -F"\t" '{print $1"\t"$10}'|awk -F":" '{print $1}'|awk -F"/" '{print $1"\t"$2}' > $REF.var.txt
+
+viruses=$(awk -F"\t" '{print $1}' <$REF.var.txt|sort|uniq )
+for v in $viruses
+do
+    x=$( grep -P "$v\t" $f.var.txt |awk -F"\t" '{if ($2!=$3) {print $1}}'|wc -l )
+    echo $REF $v $x
+done > $REF.var_count.txt
