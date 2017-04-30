670ff91cf64cc58739405a67d5a2a9758708df64
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-04-30 10:29:28 +0100

    Update analysis.R

--- paper3/scripts/analysis.R
+++ paper3/scripts/analysis.R
@@ -12,8 +12,8 @@ library(affyPLM)
 #       Load one colour Agilent data 
 #===============================================================================
 
-targets_mcb<- readTargets("targets_mcb.txt")
-targets_phd<- readTargets("targets_phd.txt")
+targets_v5<- readTargets("targets_mcb.txt")
+targets_v4<- readTargets("targets_phd.txt")
 
 get_arrays <- function(targets=targets,dpath="../data") {
 	RG <- read.maimages(
@@ -40,18 +40,20 @@ get_arrays <- function(targets=targets,dpath="../data") {
 	return(RG)
 }
 
-RG_mcb <- get_arrays(targets_mcb)
-RG_phd <- get_arrays(targets_phd)
+RG_v5 <- get_arrays(targets_v5)
+RG_v4 <- get_arrays(targets_v4)
 
-RG_norm_mcb <- backgroundCorrect(RG_mcb, method="normexp", offset=50)
-RG_norm_phd <- backgroundCorrect(RG_phd, method="normexp", offset=50)
+RG_norm_v5 <- backgroundCorrect(RG_v5, method="normexp", offset=50)
+RG_norm_v4 <- backgroundCorrect(RG_v4, method="normexp", offset=50)
+
+RG_norm_v5$G <- log2(RG_norm_v5$G)
+RG_norm_v4$G <- log2(RG_norm_v4$G)
 
 #RG_ess <- new("MAList", list(targets=RG_norm$targets, genes=RG_norm$genes, source=RG_norm$source,  A=RG_norm$G))
 
 #===============================================================================
 #	Data filtering
 #===============================================================================
-
 
 filter <- function(X) {
 	Y <- rowMeans(X)>0.5
@@ -71,26 +73,21 @@ filter_run <- function(X,n=1) {
 
 #	Y <- filter(X[,1:4])+ filter(X[,5:8])+ filter(X[,9:12])+ filter(X[,13:16])+ filter(X[,17:20])+ filter(X[,21:24])+ filter(X[,25:28])+ filter(X[,29:32])+ filter(X[,33:36])+ filter(X[,37:40])
 
+RG_filt_v5 <- RG_norm_v5[filter_run(RG_norm_v5$other$gIsWellAboveBG)>0,]
+RG_filt_v4 <- RG_norm_v4[filter_run(RG_norm_v4$other$gIsWellAboveBG,2)>0,]
 
-f1 <-  filter_run(RG_norm$other$gIsWellAboveBG) 
+RG_filt_v5 <- RG_filt_v5[RG_filt_v5$genes$ControlType==0,]
+RG_filt_v4 <- RG_filt_v4[RG_filt_v4$genes$ControlType==0,]
 
-filt <- f1>0
-
-
-RG_filt <- RG_norm[filt,]
-RG_filt <- RG_filt[RG_filt$genes$ControlType==0,]
-RG_ess <- new("MAList", list(targets=RG_filt$targets, genes=RG_filt$genes, source=RG_filt$source,  A=RG_filt$G))
-
-
-
+RG_ess_v5 <- new("MAList", list(targets=RG_filt_v5$targets, genes=RG_filt_v5$genes, source=RG_filt_v5$source,  A=RG_filt_v5$G))
+RG_ess_v4 <- new("MAList", list(targets=RG_filt_v4$targets, genes=RG_filt_v4$genes, source=RG_filt_v4$source,  A=RG_filt_v4$G))
 
 #===============================================================================
 #	    Technical Replicates
 #===============================================================================
 
 # below for v4 microarray only
-RG_ess$genes$SystematicName[grep("Agabi_varbisH97_2$",RG_ess$genes$SystematicName)] <- RG_ess$genes$ProbeName[grep("Agabi_varbisH97_2$",RG_ess$genes$SystematicName)]
-
+RG_ess_v4$genes$SystematicName[grep("Agabi_varbisH97_2$",RG_ess_v4$genes$SystematicName)] <- RG_ess_v4$genes$ProbeName[grep("Agabi_varbisH97_2$",RG_ess_v4$genes$SystematicName)]
 
 ff <- function(X) {
 	while ( (max(X) - min(X)) > 2 ) 
@@ -102,26 +99,22 @@ ff <- function(X) {
 	return(median(X))
 }
 
-
-test <- aggregate(RG_ess$A,by=list(RG_ess$genes$SystematicName),ff)
-
-E.avg <- new("MAList", list(targets=RG_filt$targets, genes=test$Group.1,source=RG_filt$source,  A=test[,2:31]))
-
+temp <- aggregate(RG_ess_v5$A,by=list(RG_ess_v5$genes$SystematicName),ff)
+E.avg.v5 <- new("MAList", list(targets=RG_filt_v5$targets, genes=temp$Group.1,source=RG_filt_v5$source,  A=temp[,2:17]))
 # for V5 only
-E.avg$genes <- sub(".*_","",E.avg$genes)
-
-E.avg.v4 <- E.avg
-E.avg.v5 <- E.avg
+E.avg.v5$genes <- sub(".*_","",E.avg.v5$genes)
+			
+temp <- aggregate(RG_ess_v4$A,by=list(RG_ess_v4$genes$SystematicName),ff)
+E.avg.v4 <- new("MAList", list(targets=RG_filt_v4$targets, genes=temp$Group.1,source=RG_filt_v4$source,  A=temp[,2:9]))
+rm(temp)
 
 #===============================================================================
 #	    Merge arrays
 #===============================================================================
 
-
 annotations <- read.csv("phd_genes.txt",header=TRUE)
 annotations <- annotations[!duplicated(annotations$Protein.ID),]
 gene_names <- read.table("gene_names.txt",sep="\t",header=TRUE) 
-
 annotations <- merge(gene_names,annotations, by.x="JGI_ID",by.y="Protein.ID",all.x=TRUE)
 
 genes_v4 <- inner_join(data_frame(Name=E.avg.v4$genes),annotations[,c(1,6)])
