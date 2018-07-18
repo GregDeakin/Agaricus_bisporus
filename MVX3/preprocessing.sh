b8823809601aab4cf82c7f7185ef3a84334088df
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-07-18 15:48:02 +0100

    Create preprocessing.sh

---
+++ MVX3/preprocessing.sh
@@ -0,0 +1,43 @@
+
+
+# filter adapters, phix and rrna
+for FR in $PROJECT_FOLDER/MVX3/fastq/*_R1.fq.gz; do
+  RR=$(sed 's/_R1/_R2/' <<< $FR);   
+  $PROJECT_FOLDER/metatranscriptomics_pipeline/scripts/PIPELINE.sh -c MEGAFILT  \
+  $PROJECT_FOLDER/metatranscriptomics_pipeline/common/resources/adapters/truseq.fa \
+  $PROJECT_FOLDER/metatranscriptomics_pipeline/common/resources/contaminants/phix_174.fa \
+  $PROJECT_FOLDER/metatranscriptomics_pipeline/common/resources/contaminants/ribokmers.fa.gz \
+  $PROJECT_FOLDER/MVX3/filtered   $FR   $RR   true; 
+done
+
+# Agaricus bisporus gene removal (bbduk)
+for FR in $PROJECT_FOLDER/MVX3/filtered/*_R1.fq.gz.filtered.fq.gz; do
+  RR=$(sed 's/_R1/_R2/' <<< $FR)
+  $PROJECT_FOLDER/metatranscriptomics_pipeline/scripts/PIPELINE.sh -c filter -p bbduk \
+  $PROJECT_FOLDER/contaminants/contaminants.fasta \
+  $PROJECT_FOLDER/MVX3/cleaner \
+  $FR \
+  $RR \
+  tossjunk \
+  k=31 \
+  t=2
+done
+
+
+# Human contaminant removal (BBMap)
+for FR in $PROJECT_FOLDER/MVX3/cleaner/*_R1.fq.gz.filtered.fq.gz.filtered.fq.gz; do
+  RR=$(sed 's/_R1/_R2/' <<< $FR)
+  $PROJECT_FOLDER/metatranscriptomics_pipeline/scripts/PIPELINE.sh -c filter -p bbmap \
+  $PROJECT_FOLDER/metatranscriptomics_pipeline/common/resources/contaminants/bbmap_human/hg19_main_mask_ribo_animal_allplant_allfungus.fa \
+  $PROJECT_FOLDER/MVX3/cleaned \
+  $FR \
+  $RR \
+  minid=0.95 \
+  maxindel=3 \
+  bwr=0.16 \
+  bw=12 \
+  quickmatch \
+  fast \
+  minhits=2 \
+  t=2
+done
